c5f62d6f03f2ac08a7e4f21699ff9b2ff422fb48
Listener Gao <gaoyashen@linglongtech.com>
2019-08-04 13:05:27 +0800

    修改主题样式

--- app/src/main/java/com/listenergao/wanandroid/base/BaseActivity.kt
+++ app/src/main/java/com/listenergao/wanandroid/base/BaseActivity.kt
@@ -2,6 +2,7 @@ package com.listenergao.wanandroid.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gyf.immersionbar.ktx.immersionBar
 
 /**
  * create on 19/07/29
@@ -12,5 +13,9 @@ open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 设置statusBar字体为深色
+        immersionBar {
+            statusBarDarkFont(true)
+        }
     }
 }
