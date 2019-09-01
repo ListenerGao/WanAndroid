6729fc554297c1d7bfe3c2866115c6fae274dd3c
Listener Gao <gaoyashen@linglongtech.com>
2019-09-01 21:17:17 +0800

    update gradle and kotlin version

--- app/src/main/java/com/listenergao/wanandroid/http/test/Test.kt
+++ app/src/main/java/com/listenergao/wanandroid/http/test/Test.kt
@@ -1,5 +1,7 @@
 package com.listenergao.wanandroid.http.test
 
+import kotlinx.coroutines.*
+
 
 fun main() {
 
@@ -19,18 +21,72 @@ fun main() {
 //    initIntArray(maxCount)
 //    initList(maxCount)
 
-    val person = Person("ListenerGao", 26)
-    val student = Student(person)
-    student.show()
+//    val person = Person("ListenerGao", 26)
+//    val student = Student(person)
+//    student.show()
+//
+//    val array = listOf(21, 40, 11, 33, 78)
+//    //过滤
+//    val list = array.filter { i ->
+//        i % 3 == 0
+//    }
+//    println(list.toString())
 
-    val array = listOf(21, 40, 11, 33, 78)
-    //过滤
-    val list = array.filter { i ->
-        i % 3 == 0
+
+//    test()
+//    test1()
+
+    GlobalScope.launch {
+        doSomeThing()
+        println("执行结束....")
     }
-    println(list.toString())
+    println("hello")
+    Thread.sleep(3000L)
+
 
 }
+
+suspend fun doSomeThing() {
+    withContext(Dispatchers.IO) {
+        Thread.sleep(2000L)
+        println("world")
+    }
+}vw
+
+fun test() = runBlocking {
+    val job = GlobalScope.launch {
+        // 启动一个新协程并保持对这个作业的引用
+        delay(1000L)
+        println("World!")
+        println("0 current thread : ${Thread.currentThread()}")
+    }
+    println("Hello,")
+    println("1 current thread : ${Thread.currentThread()}")
+    job.join() // 等待直到子协程执行结束
+    println("2 current thread : ${Thread.currentThread()}")
+}
+
+fun test1() = runBlocking {
+    // this: CoroutineScope
+    launch {
+        delay(200L)
+        println("Task from runBlocking")
+    }
+
+    coroutineScope {
+        // 创建一个协程作用域
+        launch {
+            delay(500L)
+            println("Task from nested launch")
+        }
+
+        delay(100L)
+        println("Task from coroutine scope") // 这一行会在内嵌 launch 之前输出
+    }
+
+    println("Coroutine scope is over") // 这一行在内嵌 launch 执行完毕后才输出
+}
+
 
 fun initArray(size: Int) {
     val startTime = System.currentTimeMillis()
